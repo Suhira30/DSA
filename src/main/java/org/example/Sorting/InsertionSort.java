afeee6970ecf61df99830df08c4f6eb4d8b482b2
Suhira Balarajan <suhira@smartzi.com>
2026-01-16 16:02:25 +0530

    Coding: Insertion sort

--- src/main/java/org/example/Sorting/InsertionSort.java
+++ src/main/java/org/example/Sorting/InsertionSort.java
@@ -1,5 +1,63 @@
 package org.example.Sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class InsertionSort {
+    //Way :1 -GOOD
+    public void insertionSortingBestWay(int[] array){
+        System.out.println("Before sorting :" + Arrays.toString(array));
+        for (int i = 1; i < array.length; i++){
+            int key = array[i];
+            int j = i - 1;
+            while(j>=0 && array[j] > key){
+                array[j+1]=array[j];
+                j--;
+            }
+            array[j+1]=key;
+        }
+        System.out.println("After sorting :" + Arrays.toString(array));
+    }
+    //way :2
+    public void insertionSorting(int[] array) {
+        System.out.println("Before sorting :" + Arrays.toString(array));
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int currentIndex = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (key < array[j]) {
+                    array[j + 1] = array[j];//shift
+                    currentIndex = j;
+                } else {
+                    break;
+                }
+            }
+            array[currentIndex] = key;
+        }
+        System.out.println("After sorting :" + Arrays.toString(array));
+    }
+
+    //way 3:
+    public void insertionSortUsingAddRemove(int[] array) {
+        System.out.println("Before sorting :" + Arrays.toString(array));
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i : array) {
+            arrayList.add(i);
+        }
+        for (int i = 1; i < arrayList.size(); i++) {
+            int currentValue = arrayList.remove(i);
+            int insertIndex = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arrayList.get(j)> currentValue) {
+                    insertIndex = j;
+                }
+                else {
+                    break;
+                }
+            }
+            arrayList.add(insertIndex, currentValue);
+        }
+        System.out.println("After sorting :" + arrayList);
+    }
 
 }
--- src/main/java/org/example/Sorting/InsertionSortDemo.java
+++ src/main/java/org/example/Sorting/InsertionSortDemo.java
@@ -7,6 +7,18 @@ public class InsertionSortDemo {
     public static void main(String[] args) {
         //INSERTION SORT--------------------------------------------------------------------------------
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        System.out.println("Enter the elements of the array: ");
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+        int[] arr3=array.clone();
+        int[] arr4=array.clone();
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSorting(array);
+        insertionSort.insertionSortingBestWay(arr4);
+        insertionSort.insertionSortUsingAddRemove(arr3);
     }
 }
