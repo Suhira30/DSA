8931c64707e767c0184df3eddd869ab3af68ebae
Suhira Balarajan <suhira@smartzi.com>
2026-01-16 12:04:25 +0530

    Coding: Insertion sort

---
+++ src/main/java/org/example/Sorting/InsertionSortDemo.java
@@ -0,0 +1,12 @@
+package org.example.Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSortDemo {
+
+    public static void main(String[] args) {
+        //INSERTION SORT--------------------------------------------------------------------------------
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
