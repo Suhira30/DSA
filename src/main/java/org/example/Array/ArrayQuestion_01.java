86ca71bd83f385acff02ce4d4edcac98d3ee80dc
Suhira30 <suhirabalarajan@gmail.com>
2026-01-17 23:08:16 +0530

    PrbSolving: Array 01

---
+++ src/main/java/org/example/Array/ArrayQuestion_01.java
@@ -0,0 +1,20 @@
+package org.example.Array;
+
+public class ArrayQuestion_01 {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }else{
+                count=0;
+                continue;
+            }
+            if(max<count){
+                max=count;
+            }
+        }
+        return max;
+    }
+}
