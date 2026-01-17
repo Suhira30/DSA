4ce7aa5e6fe1e9c808f4dd79d9cb0c0428d16503
Suhira30 <suhirabalarajan@gmail.com>
2026-01-17 23:09:14 +0530

    PrbSolving|refactor: Array 01

--- src/main/java/org/example/Array/ArrayQuestion_01.java
+++ src/main/java/org/example/Array/ArrayQuestion_01.java
@@ -4,15 +4,15 @@ public class ArrayQuestion_01 {
     public int findMaxConsecutiveOnes(int[] nums) {
         int count=0;
         int max=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1){
+        for (int num : nums) {
+            if (num == 1) {
                 count++;
-            }else{
-                count=0;
+            } else {
+                count = 0;
                 continue;
             }
-            if(max<count){
-                max=count;
+            if (max < count) {
+                max = count;
             }
         }
         return max;
