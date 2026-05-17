2929b2fb9b80387be06e05bfe92798515faca4d1
Suhira30 <suhirabalarajan@gmail.com>
2026-05-17 21:38:52 +0530

    feat:StackDemo.java class created

---
+++ src/main/java/org/example/Stack/StackDemo.java
@@ -0,0 +1,19 @@
+package org.example.Stack;
+
+import java.util.Stack;
+
+public class StackDemo {
+public static void main(String[] args) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+
+    System.out.println(stack);
+    System.out.println(stack.peek());
+    System.out.println(stack.empty());
+    System.out.println(stack.pop());
+    System.out.println(stack.peek());
+}
+}
