be14fc5be5b9be7a681a4db26f84f46b09950d78
Suhira30 <suhirabalarajan@gmail.com>
2026-05-21 23:34:38 +0530

    feat:deletion min in tree

--- src/main/java/org/example/Tree/BST.java
+++ src/main/java/org/example/Tree/BST.java
@@ -61,4 +61,59 @@ public class BST {
         }
         return root.data;
     }
+
+    //Deletion
+    public Node delete(Node root, int value) {
+        BST bst = new BST();
+        // STEP 1: find the node
+        if (root == null) {
+            return null;
+        }
+
+        // go left
+        if (value < root.data) {
+            root.left = delete(root.left, value);
+        }
+
+        // go right
+        else if (value > root.data) {
+            root.right = delete(root.right, value);
+        }
+
+        // STEP 2: node found
+        else {
+
+            // CASE 1: Leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // CASE 2: One child (right only)
+            else if (root.left == null) {
+                return root.right;
+            }
+
+            // CASE 2: One child (left only)
+            else if (root.right == null) {
+                return root.left;
+            }
+
+            // CASE 3: Two children
+            else {
+
+                // find inorder successor (smallest in right subtree)
+                int minValue = bst.minimum(root.right);
+
+                // replace current node value
+                root.data = minValue;
+
+                // delete duplicate from right subtree
+                root.right = delete(root.right, minValue);
+            }
+        }
+
+        return root;
+    }
+
+
 }
