8783b5027db615310e56dfbfd18ae7eb83a294fd
Suhira30 <suhirabalarajan@gmail.com>
2026-05-25 23:28:30 +0530

    feat:fibonacci.java created and impl

---
+++ src/main/java/org/example/recursion/Fibonacci.java
@@ -0,0 +1,15 @@
+package org.example.recursion;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+    }
+
+    public static int fibonacci(int n) {
+        //0,1,1,2,3,5.............
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
