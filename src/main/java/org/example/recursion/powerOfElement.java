e5d3a47f68d111dfb922a8c8338268caed3efe4d
Suhira30 <suhirabalarajan@gmail.com>
2026-05-25 23:28:15 +0530

    feat:powerOfElement.java created and impl

---
+++ src/main/java/org/example/recursion/powerOfElement.java
@@ -0,0 +1,14 @@
+package org.example.recursion;
+
+public class powerOfElement {
+public static void main(String[] args) {
+    System.out.println(powerOfElement(2,3));
+}
+public static int powerOfElement(int n,int x)
+{
+    if(n==0){
+        return 1;
+    }
+    return x*powerOfElement(n-1,x);
+}
+}
