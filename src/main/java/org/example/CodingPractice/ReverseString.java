2833925c169ad899c370c51a1e2dce676e360c45
Suhira30 <suhirabalarajan@gmail.com>
2026-05-27 02:52:59 +0530

    chore: warning resolved

--- src/main/java/org/example/CodingPractice/ReverseString.java
+++ src/main/java/org/example/CodingPractice/ReverseString.java
@@ -3,7 +3,7 @@ package org.example.CodingPractice;
 import java.util.Scanner;
 
 public class ReverseString {
-public static void main(String args[])
+public static void main(String[] args)
 {
 	Scanner sc=new Scanner(System.in);
     System.out.println("Enter a string:");
@@ -33,11 +33,11 @@ public static  String reverse(String str)
 
 public static   String reverseUsingLoop(String str)
 {
-    String result="";
+    StringBuilder result= new StringBuilder();
   for(int i=str.length()-1;i>=0;i--){
-      result+=str.charAt(i);
+      result.append(str.charAt(i));
   }
-  return result;
+  return result.toString();
 }
 }
         /*
