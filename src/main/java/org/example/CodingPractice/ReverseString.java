cfe43a60ab5ca168d0e93b442fe3625626143c72
Suhira30 <suhirabalarajan@gmail.com>
2026-05-29 01:14:19 +0530

    chore:modified

--- src/main/java/org/example/CodingPractice/ReverseString.java
+++ src/main/java/org/example/CodingPractice/ReverseString.java
@@ -17,9 +17,6 @@ public static void main(String[] args)
     //method 2
     System.out.println(reverse(str));
 
-    //method 3 : using loop
-    System.out.println("reverse using loop:"+reverseUsingLoop(str));
-
 }
 public static  String reverse(String str)
 {
@@ -31,14 +28,7 @@ public static  String reverse(String str)
     return sb.toString();
 }
 
-public static   String reverseUsingLoop(String str)
-{
-    StringBuilder result= new StringBuilder();
-  for(int i=str.length()-1;i>=0;i--){
-      result.append(str.charAt(i));
-  }
-  return result.toString();
-}
+
 }
         /*
         ** space is definitely considered a character in Java strings
